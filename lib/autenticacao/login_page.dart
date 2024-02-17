8df56431794eb7149f932e57e762bae3e4e4cb40
TarsisGuerra <tarsisguerra@hotmail.com>
2024-02-16 21:34:21 -0300

    telaLogin

--- lib/autenticacao/login_page.dart
+++ lib/autenticacao/login_page.dart
@@ -10,8 +10,99 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(),
+    return Scaffold(
+      backgroundColor: const Color(0xFF333333),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 200),
+        child: Column(
+          children: [
+            const Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Acessar',
+                style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30, color: Colors.white),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                height: 55,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:  BorderRadius.circular(10), 
+                  ),
+                  child: const Align (
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'E-mail',
+                      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16, color: Colors.grey),
+                    ),
+                    ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                height: 55,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:  BorderRadius.circular(10)
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Senha',
+                        style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16, color: Colors.grey),
+                      ),
+                      Text(
+                        'Mostrar',
+                        style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16, color: Colors.black),
+                      ),
+                    ],
+                  ),
+                  
+              ),
+            ),
+            const SizedBox(height: 50),
+            botao(),
+          const SizedBox(height: 30),
+          
+          const Text(
+            'Esqueceu a senha?',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white),
+          )
+          ],
+        ),
+      ),
     );
+  }
+
+  botao(){
+   return  Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                height: 55,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF272020),
+                  borderRadius:  BorderRadius.circular(10), 
+                  ),
+                  child: const Align (
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Acessar',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white),
+                    ),
+                    ),
+              ),
+            );
   }
 }
